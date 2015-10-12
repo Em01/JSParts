06d23d525f53a8f7f41b3c4425bfd81f4c854299
Em01 <emma_williams1@hotmail.co.uk>
2015-10-12 20:15:55 +0100

    By value and by reference

--- js_weird_parts/byValueByReference/app.js
+++ js_weird_parts/byValueByReference/app.js
@@ -3,3 +3,17 @@ var a = 3;
 var b;
 
 b = a;
+a = 2;
+
+console.log(a);
+console.log(b);
+
+// by reference ( all objects ( including functions ))
+var c = { greeting: 'hi' };
+var d;
+
+d = c;
+c.greeting = 'hello'; //mutate
+
+console.log(c);
+console.log(d);
