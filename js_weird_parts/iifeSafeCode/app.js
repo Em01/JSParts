bf6bfce288e2ba14b84e0b8d38e2549bfa896646
Em01 <emma_williams1@hotmail.co.uk>
2015-10-20 17:54:38 +0100

    IIFEs and safe

--- js_weird_parts/iifeSafeCode/app.js
+++ js_weird_parts/iifeSafeCode/app.js
@@ -1,1 +1,19 @@
-app.js
+// IFIE
+(function(name) {
+
+	var greeting = 'Hello';
+	console.log(greeting + ' ' + name);
+
+}('John')); //IIFE
+
+
+//Pass global object into the IIFE
+// (function(global, name) {
+
+// 	var greeting = 'Hello';
+// 	global.greeting = 'Hello'; //intentionally affect the global object hola
+// 	console.log(greeting + '' + name);
+
+// }(window, 'John')); 
+
+// console.log(greeting);
