d483b63c83a9c9992f1cd3b4a4f91421ce38b8f1
Em01 <emma_williams1@hotmail.co.uk>
2015-10-21 11:36:59 +0100

    More Function Closures

--- js_weird_parts/AdditionalClosures/app.js
+++ js_weird_parts/AdditionalClosures/app.js
@@ -28,3 +28,37 @@ fs[2]();
 //3
 //3
 //3
+
+
+
+
+
+//If you want access to the other variables
+
+
+function buildFunctions2() {
+
+	var arr = [];
+
+	for (var i = 0; i < 3; i++) {
+		arr.push(
+			(function(j) {
+				return function() {
+					console.log(j);
+				};
+			}(i))
+		)
+	}
+
+	return arr;
+
+}
+
+
+var fs2 = buildFunctions2();
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
+
+
