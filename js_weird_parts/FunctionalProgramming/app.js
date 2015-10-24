1058ba2f170859db76404235d0b1d3e2ea573a81
Em01 <emma_williams1@hotmail.co.uk>
2015-10-24 14:02:56 +0100

    Functional programming and bind with alternative

--- js_weird_parts/FunctionalProgramming/app.js
+++ js_weird_parts/FunctionalProgramming/app.js
@@ -24,3 +24,21 @@ var arr3 = mapForEach(arr1, function(item) {
 });
 
 console.log(arr3);
+
+
+
+var checkPastLimit = function(limiter, item) {
+  return item > limiter;
+}
+var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+console.log(arr4);
+
+
+var checkPastLimitSimplified = function(limiter) {
+  return function(limiter, item) {
+  return item > limiter;
+  }.bind(this, limiter);
+};
+
+var arr5 = mapForEach(arr1, checkPastLimitSimplified(2));
+console.log(arr5);
