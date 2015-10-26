22cef1601e12b36a49e29a1e39550fa66c2f78d3
Em01 <emma_williams1@hotmail.co.uk>
2015-10-26 19:22:08 +0000

    The prototype

--- js_weird_parts/Prototype/app.js
+++ js_weird_parts/Prototype/app.js
@@ -1,1 +1,25 @@
-app.js
+var person = {
+	firstname: 'Default',
+	lastname: 'Default',
+	getFullName: function() {
+		return this.function + ' ' + this.lastname;
+	}
+}
+
+var john = {
+	firstname: 'John',
+	lastname: 'Doe'
+}
+
+
+// do not do this EVER!!! demo purpose only.
+john.__proto__ = person;
+console.log(john.getFullName());
+console.log(john.firstname);
+
+var jane = {
+	firstname: 'Jane'
+}
+
+jane.__proto__ = person;
+console.log(jane.getFullName());
