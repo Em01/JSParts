6e96e6e8c4657614b44b9b8502851814b1f57ca0
Em01 <emma_williams1@hotmail.co.uk>
2015-11-30 15:37:39 +0000

    Additional objects

--- objectOriented/aquariumObjects.js
+++ objectOriented/aquariumObjects.js
@@ -18,3 +18,12 @@ aquarium.takeOut = function(name){
 
 var fishOutOfWater = aquarium.takeOut("Marlin");
 console.log( fishOutOfWater);
+
+
+lighthouseRock.addBulb = function(name, wattage) {
+  this.weaponBulbs.push([name, wattage]);
+};
+
+lighthouseRock.addBulb("Blasterbright", 5000);
+lighthouseRock.addBulb("Sight Slayer", 1800);
+lighthouseRock.addBulb("Burner of Souls", 7500);
