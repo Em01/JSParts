89f9e445e2bc329caa00acca50447f2358a74919
Em01 <emma_williams1@hotmail.co.uk>
2016-01-25 21:31:30 +0000

    Boolean and Bitwise operators

--- Basics/operators.js
+++ Basics/operators.js
@@ -27,3 +27,40 @@ var total = 9 / "3";
 var level;
 console.log(++level);
 //NaN
+
+var value = -9;
+value = +value;
+console.log(value);
+// -> returns -9
+
+var num1 = parseInt('1010', 2);
+var num2 = parseInt('0110', 2);
+var total = num1 & num2;
+console.log(total.toString(2));
+//10
+
+var num1 = parseInt('0001', 2);
+num1 = num1 << 2;
+console.log(num1.toString(2));
+//100
+
+var num1 = -parseInt('1000', 2);
+num1 = num1 >> 3;
+console.log(num1.toString(2));
+//-1
+
+//!! converts to boolean value
+var value = !!false;
+conole.log(value);
+//false
+
+var obj = {
+  calc: 'Logic'
+};
+var value = obj && 99;
+console.log(value);
+//99 as second operand
+
+var value = null && 99;
+console.log(value);
+//null
