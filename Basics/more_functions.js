6e74c0cea4f81c51c15daf1039c0a715828f5fc2
Em01 <emma_williams1@hotmail.co.uk>
2016-01-30 19:21:17 +0000

    Initial thumbslider

---
+++ Basics/more_functions.js
@@ -0,0 +1,6 @@
+var Employee = function(name) {
+  this.name = name;
+};
+var newEmployee = new Employee('jj');
+console.log(typeof newEmployee);
+//function
--- objectOriented/properties.js
+++ objectOriented/properties.js
@@ -127,3 +127,5 @@ console.log(descriptor);
   writable: false
 }
 */
+
+
