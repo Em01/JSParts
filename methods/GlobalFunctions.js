f8bd2bd14bd1fa06332823fe7f0231ff07f58dd0
Em01 <emma_williams1@hotmail.co.uk>
2016-02-09 19:43:07 +0000

    Global Functions

---
+++ methods/GlobalFunctions.js
@@ -0,0 +1,15 @@
+//parseInt()
+var value = parseInt('1234'); //1234
+
+var value = parseInt('b1234'); //NaN
+
+var value = parseFloat('123.9'); //123.9
+
+var value = isFinite(Number.POSITIVE_INFINITY); //false
+var value = isFinite(42); //true
+
+var value = isNaN(NaN); //true
+
+var path = "\\start\\";
+encodeURI(path); //%5Cstart%5c
+
