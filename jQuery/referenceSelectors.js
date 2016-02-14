a13cb8f7501eca2f1387b66678b82f927f7ddf85
Em01 <emma_williams1@hotmail.co.uk>
2016-02-14 18:37:40 +0000

    Id selectors

---
+++ jQuery/referenceSelectors.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+});
+
+$(document).ready(function() {
+  $('div, span').css('propName', 'value');
+});
+
+$(document).ready(function() {
+  $('div').each(function() {
+    alert($(this).html());
+  });
+});
+
+$('#myID');
+
+$document.ready(function() {
+  alert($('#myDiv').html());
+});
+//could do:
+$(document).ready(function() {
+  document.getElementById('TestDiv');
+});
