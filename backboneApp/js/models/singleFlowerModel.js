a45d2658cc6008673fcc4f62135c8ed3d2025590
Em01 <emma_williams1@hotmail.co.uk>
2016-02-21 14:38:50 +0000

    Clean up model

--- backboneApp/js/models/singleFlowerModel.js
+++ backboneApp/js/models/singleFlowerModel.js
@@ -9,20 +9,4 @@ app.singleFlower = Backbone.Model.extend({
     color: "pink",
     img: "images/placeholder.svg"
   },
-//watch for a change to a model instance property.
-  initialize: function() {
-    console.log("A model instance name " + this.get("name") + " has been created and it costs " + this.get("price"));
-   
-    this.on('change', function() {
-      console.log("Something in our model has changed");
-    });
-
-    this.on('change', function() {
-      console.log('The ' + this.get("name") + " model instance just changed!");
-    });
-
-    this.on('change:price', function(){
-      console.log("The price for the " + this.get('name') + "model just changed to $" + this.get("price") + " dollars");
-    });
-  }
 });
