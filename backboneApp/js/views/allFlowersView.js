6b5acf9498c787fb5ec8efff493e8cb4c77d1408
Em01 <emma_williams1@hotmail.co.uk>
2016-02-21 18:00:54 +0000

    Collection view

---
+++ backboneApp/js/views/allFlowersView.js
@@ -0,0 +1,16 @@
+var app = app || {};
+
+app.allFlowersView = Backbone.View.extend({
+  
+  tagName: "section",
+  
+  render: function() {
+    this.collection.each(this.addFlower, this);
+    return this;
+  },
+
+  addFlower: function() {
+    var flowerView = new app.singleFlowerView({ model: flower});
+    this.$el.append(flowerView.render().el);
+  }
+});
