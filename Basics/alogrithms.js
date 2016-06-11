fd4eb1182d56f3f31d3712f4dd5be7692d9cb1f2
Em01 <emma_williams1@hotmail.co.uk>
2016-06-11 20:40:21 +0100

    Clean

--- Basics/alogrithms.js
+++
@@ -1,8 +0,0 @@
-//sum all elements within a value
-function summation (num) {
-	total = 0;
-	for(i = 0; i <= total; i++ ) {
-		total += i;
-	}
-	return total;
-}
--- john_pollock_excercises/ex1.html
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<script type="text/javascript">
-document.write("This was written in JavaScript");
-</script>
