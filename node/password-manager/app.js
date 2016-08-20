0ec48f971ec28ee23a8e1a1fd32da1b574294c97
Em01 <emma_williams1@hotmail.co.uk>
2016-08-20 23:55:52 +0100

    Yargs

--- node/password-manager/app.js
+++ node/password-manager/app.js
@@ -1,8 +1,46 @@
 console.log('starting password manager');
 
 var storage = require('node-persist');
-
 storage.initSync();
+
+var argv = require('yargs')
+  .command('create', 'Creates the user', function(yargs){
+    yargs.options({
+      name:{
+        demand: true,
+        alias: 'n',
+        type: 'string',
+        description: 'Your first name'
+      },
+      username:{
+        demand: 'true',
+        alias: 'u',
+        type: 'string',
+        description: 'Your username'
+      },
+      password:{
+        demand: 'true',
+        alias:'p',
+        type: 'string',
+        description: 'Your password'
+      }
+    }).help('help');
+  })
+  .command('get', 'Gets the user', function(){
+    yargs.options({
+      name:{
+        demand: 'true',
+        alias: 'n',
+        type: 'string',
+        description: 'Your name'
+      }
+    }).help('help');
+  })
+  .help('help')
+  .argv;
+
+
+var command = argv._[0];
 
 function createAccount(account){
   var accounts = storage.getItemSync('accounts');
@@ -30,13 +68,21 @@ function getAccount(accountName){
   });
   return matchedAccount;
 }
+if (command == 'create'){
+  var createdAccount = createAccount({
+    name: argv.name,
+    username: argv.username,
+    password: argv.password
+  });
+  console.log('Account created!');
+  console.log(createdAccount);
+} else if(command === 'get') {
+  var fetchedAccound = getAccount(argv.name);
 
-/*createAccount({
-  name: 'Test',
-  username: 'tester',
-  password: '123'
-});
-*/
-
-var testAccount = getAccount('Test');
-console.log(testAccount);
+  if(typeof fetchedAccount === 'undefined') {
+    console.loh('Account not found');
+  } else {
+    console.log('Account found!');
+    conaole.log(fetchedAccount);
+  }
+}
