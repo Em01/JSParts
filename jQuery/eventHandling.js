af040f04b9ee2d6887b6d6e5a5a28627310928ea
Em01 <emma_williams1@hotmail.co.uk>
2016-09-02 10:12:12 +0700

    jquery events

--- jQuery/eventHandling.js
+++ jQuery/eventHandling.js
@@ -1,3 +1,13 @@
 $('.test').on('click', 'button', function(){
 
 });
+
+//preventDefault to prevent the default behaviour of the browser.
+//stopPropagation- stops events from bubbling.
+$(document).ready(function(){
+  $('.vacation').on('click', '.expand',
+    function(event) {
+      event.preventDefault();
+    }
+  );
+});
