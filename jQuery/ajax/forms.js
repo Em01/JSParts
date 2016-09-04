b6024e0088fd1c3fe6cb6b5b84ce7e42d058c6be
Em01 <emma_williams1@hotmail.co.uk>
2016-09-04 18:21:56 +0700

    ajax forms

--- jQuery/ajax/forms.js
+++ jQuery/ajax/forms.js
@@ -1,5 +1,6 @@
 //preventDefault will prevent the browser submitting.
 //serialize will merge all fields for the submission.
+//attr(<attribute>, <value>)
 
 $('form').on('submit', function(event){
   event.preventDefault();
