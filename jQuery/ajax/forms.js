221c84f2bff0fa5fd8bcefdee904b4ad986e6f90
Em01 <emma_williams1@hotmail.co.uk>
2016-09-04 14:49:31 +0700

    ajax forms

---
+++ jQuery/ajax/forms.js
@@ -0,0 +1,13 @@
+//preventDefault will prevent the browser submitting.
+//serialize will merge all fields for the submission.
+
+$('form').on('submit', function(event){
+  event.preventDefault();
+  var form = $(this);
+  $.ajax('/book', {
+    type: 'POST',
+    data: form.serialize(),
+    success: function(results){
+    }
+  });
+});
---
+++ jQuery/ajax/requests.js
@@ -0,0 +1,9 @@
+//Simple HTTP Request
+
+/*
+ * GET: /files.txt
+ * Host: myhost.net
+ * User-agent: some browser
+
+// Sample response
+// 
