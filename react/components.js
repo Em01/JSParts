03e095541b92c9604cb9f0cab1fa42c23a5eb6e4
Em01 <emma_williams1@hotmail.co.uk>
2017-01-11 09:06:46 +0700

    clean

--- react/components.js
+++ react/components.js
@@ -19,3 +19,15 @@ ReactDOM.render(
 //to make a component class use React.createClass-store this in a var.
 //to make an instance
 <myComponent />
+
+//class based component is used whenever we want to have the concept of state
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+}
+
+//functional components are more static
+const myComponent = (props) => {}
