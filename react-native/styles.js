8428e8363d7c2bdbe75699543fcfe01dd9af7243
Em01 <emma_williams1@hotmail.co.uk>
2017-02-01 17:02:05 +0000

    create react native cheat stylesheet

---
+++ react-native/styles.js
@@ -0,0 +1,24 @@
+/*
+ * Container Styling-if added to the parent e.g (View) it will amend the child
+ * justifyContent:
+ * adjusts the elements vertical direction
+ * flex-end-to bottom
+ * center
+ * flex-start-top(usually this is the default)
+ *
+ * alignItems: 
+ * adjusts the elements horizontal direction
+ * start(left)
+ * center
+ * end(right)
+ *
+ *
+ * SHADOW:
+ * shadowColor: #000
+ * shadowOffset(defines shadow dimensions): {height: 2, width: 0}
+ * shadowOpacity: 0.2
+ *
+ * ELEVATION:
+ *
+ *
+ */
