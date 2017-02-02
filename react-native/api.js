ae9c0ab73361636bd083e11ff7e8e14329fff156
Em01 <emma_williams1@hotmail.co.uk>
2017-02-02 17:57:19 +0000

    secureTextEntry

--- react-native/api.js
+++ react-native/api.js
@@ -12,3 +12,37 @@
 //
 
 //Linking-link user over to a browser
+
+//TEXT INPUT-like Image render with 0 height and width
+//itself is not responsible for knowing its value we have to tell it its value
+//value={this.state.text}
+//onChangeText={text => this.setState({ text }) -called when user types input
+//
+
+/*
+     const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles
+  return (
+    <View style = {containerStyle}>
+      <Text style = {labelStyle}>{label}</Text>
+      <TextInput
+        secureTextEntry={secureTextEntry}
+        style = {inputStyle}
+        value={value}
+        onChangeText={onChangeText}
+        autoCorrect={false}
+        placeholder={placeholder}>
+      </TextInput>
+    </View>
+    
+    Note taken props from:
+      <Input
+            secureTextEntry            
+            label="Email"
+            placeholder="user@gmail.com" 
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+      />
+
+      secureTextEntry for passwords
+*/
