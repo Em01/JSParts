a6153d27a0881b9c9d6a74cee6cd48c1be86879e
Em01 <emma_williams1@hotmail.co.uk>
2017-02-02 14:07:03 +0000

    Further react native

---
+++ react-native/api.js
@@ -0,0 +1,11 @@
+//ScrollView = for scrolling content
+//When you have this you must have flex 1 for index root element
+//
+
+//Touchable-BUTTONS-components for user presses differing on how they handle providing
+//these also come with click handlers
+//feedback to the user.
+//Highlight
+//NativeFeedback
+//Opacity -fades on click
+//WithoutFeedback
