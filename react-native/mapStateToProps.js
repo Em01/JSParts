278967b1b60d5d0bd944e314235d6422f8cabd1b
Em01 <emma_williams1@hotmail.co.uk>
2017-02-05 11:31:30 +0000

    MapStateToProps

---
+++ react-native/mapStateToProps.js
@@ -0,0 +1,5 @@
+const mapStateToProps (state, ownProps) {
+  //maps the state to the props!
+  //ownProps is the props which are passed to the component that we are wrapping-
+  //exactly equal to this.props inside of the component
+}
