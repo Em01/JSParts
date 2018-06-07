2f715e834a65ab5e5700a61d931fd61721c23870
Emma Williams <github@em01.co.uk>
2018-06-07 23:41:52 +0700

    Restful

---
+++ node/restful.js
@@ -0,0 +1,14 @@
+/*
+
+Client & server communicate using HTTP protocol.
+On the server we expose a bunch of services which are accessible via the http protocol.
+The client can then call these services using http requests.
+
+REST= Representational State Transfer
+
+endpoints are resources e.g api/weather
+
+CRUD = Create Read Update Delete
+
+HTTP Methods - Put Post Get Delete
+*/
