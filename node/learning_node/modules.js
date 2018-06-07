ab7eb76e22fa88b5708e3ded9320b1549c788de2
Emma Williams <github@em01.co.uk>
2018-06-07 13:45:31 +0700

    Further modules

--- node/learning_node/modules.js
+++ node/learning_node/modules.js
@@ -12,4 +12,11 @@ modules means that you won't override other functions due to encapsulation.
 Each program has one file that you would consider to be the main module (e.g app.js)
 A module is not a global object, The variables defined within the module are scoped
 to that module.
+
+Path: Gives you access to utilities for accessubg and working with file and directory paths.
+
+OS: For working with the operating system.
+
+Filesystem: For working with Files and directories. Almost every operation here is wither synchronous and blocking
+or asynchronous and non-blocking.
 */
---
+++ node/path/app.js
@@ -0,0 +1,4 @@
+//Path provides access to utilities for working with and accessing file paths.
+const path = require('path');
+
+var pathObj = path.parse(__filename);
