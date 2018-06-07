86f5dd213391f074c2db7318a2d7d66fe30e0f4c
Emma Williams <github@em01.co.uk>
2018-06-07 08:01:27 +0700

    Further node notes.

---
+++ node/global-object.js
@@ -0,0 +1,11 @@
+/*
+Node uses the global object as opposed to the window.
+
+* setTimeout()/clearTimeout()/setInterval()/clearInterval()
+* console.log could be accessed as global.console.log
+
+* Variables defined in the global scope are not set on the global
+object due to the modular system that node uses.
+
+
+*/
---
+++ node/learning_node/modules.js
@@ -0,0 +1,15 @@
+/*
+MODULES:
+* http
+* filesystem
+* os
+* events
+
+
+
+* Using a module and defining variables within the scope of these
+modules means that you won't override other functions due to encapsulation.
+Each program has one file that you would consider to be the main module (e.g app.js)
+A module is not a global object, The variables defined within the module are scoped
+to that module.
+*/
