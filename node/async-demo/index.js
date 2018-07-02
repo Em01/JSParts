a40d02410751dae57cd1489537773400a244c5ef
Emma Williams <github@em01.co.uk>
2018-07-02 14:44:40 +0700

    Async/await

--- node/async-demo/index.js
+++ node/async-demo/index.js
@@ -1,17 +1,33 @@
 console.log('Before');
 
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log('Commits', commits))
-  .catch(err => console.log('Error', err.message));
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commits', commits))
+//   .catch(err => console.log('Error', err.message));
 
   console.log('After');
+
+  //Async and Await approach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  catch(err) {
+    console.log(err.message)
+  }
+}
+
+displayCommits()
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('reading from a db')
+      // reject(new Error('error'))
       resolve({ id: id, gitHubUsername: 'em' })
     }, 2000);
   })
