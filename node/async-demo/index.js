683a24f9189fe2cbdf03eda2d2327ae2973f2eb3
Emma Williams <github@em01.co.uk>
2018-07-02 09:39:31 +0700

    Replace promises with callbacks

--- node/async-demo/index.js
+++ node/async-demo/index.js
@@ -1,5 +1,43 @@
 console.log('Before');
 
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+})
+
+console.log('After')
+
+function getUser(id) {
+  return new Promise((resolve, reject => {
+    setTimeout(() => {
+      console.log('reading from a db')
+      resolve({ id: id, gitHubUsername: 'em'})
+    }, 2000);
+  })
+}
+
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('calling api')
+      resolve(['repo1', 'repo2', 'repo3'])
+    }, 2000)
+  })
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('calling api')
+      resolve(['repo1', 'repo2', 'repo3'])
+    }, 2000)
+  })
+}
+
 getUser(1, getRepositories(user.gitHubUsername, getCommits);
 
 console.log('After');
