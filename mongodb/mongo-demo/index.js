38f396440d7f773778e75ade9ba03b0b205e0e89
Emma Williams <github@em01.co.uk>
2018-07-05 21:50:20 +0700

    Basic queries.

--- mongodb/mongo-demo/index.js
+++ mongodb/mongo-demo/index.js
@@ -32,5 +32,13 @@ async function createCourse() {
   console.log(result);
 }
 
+async function getCourses() {
+  const courses = await Course
+  .find({ author: 'Mosh', isPublished: true })
+  .limit(10)
+  .sort({ name: 1 }) //1 indicates ascending order, -1 is descending
+  .select({ name: 1, tags: 1})
+  console.log(courses)
+}
 
-createCourse();
+getCourses();
