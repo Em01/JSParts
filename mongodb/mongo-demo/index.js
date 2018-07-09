1bbbce8d58f933cc0401ba7870bbd21b1ad70c26
Emma Williams <github@em01.co.uk>
2018-07-09 09:18:19 +0700

    Async validators mongo

--- mongodb/mongo-demo/index.js
+++ mongodb/mongo-demo/index.js
@@ -14,8 +14,14 @@ const courseSchema = new mongoose.Schema({
   tags: {
     type: Array,
     validate: {
-      validator: function(v) {
-        return v.length > 0
+      isAsync: true,
+      validator: function(v, callback) {
+        setTimeout(() => {
+          //filesystem etc async work
+          const result = v && v.length > 0
+          callback(result)
+        }, 4000)
+        return
       },
       message: 'A course should have at least one tag.'
     }
